c9a1a9a5b0abc5f23ed9f938b09e383a1bf7ad25
Artreus <81832000+Artreus@users.noreply.github.com>
2023-07-06 19:04:38 +0300

    Add files via upload

---
+++ src/main/java/Animal.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Animal {
+    public static void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Cat extends Animal {
+    public static void meow() {
+        System.out.println("meowing...");
+    }
+}
